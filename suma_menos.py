7427f24d23f2ba6153888e8669120fe0ab8c8ad2
Leonardo Alfredo Sordo Muñiz <a01714767@tec.mx>
2025-08-26 18:05:59 -0600

    Add files via upload

---
+++ suma_menos.py
@@ -0,0 +1,7 @@
+#Un algoritmo que suma dos valores numerico
+num_a = int(input("Escribe el numero a: "))
+num_b = int(input("Escribe el numero b: "))
+
+resultado = num_a + num_b 
+
+print(f"El resultado es: {resultado}")
